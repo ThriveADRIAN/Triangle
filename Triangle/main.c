ee3363ad7141183210b095d791281044a72d507f
Adrian Cunanan <adrian.cunanan@me.com>
2013-04-05 16:51:20 -0400

    Challenge Completed

--- Triangle/main.c
+++ Triangle/main.c
@@ -8,11 +8,22 @@
 
 #include <stdio.h>
 
+float totalTriangle = 180.0;
+
+float remainingAngle(float firstAngle, float secondAngle)
+{
+    float thirdAngle;
+    thirdAngle = totalTriangle - firstAngle - secondAngle;
+    return thirdAngle;
+}
+
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    float angleA = 30.0;
+    float angleB = 60.0;
+    float angleC = remainingAngle(angleA, angleB);
+    printf("The third angle is %.2f\n", angleC);
     return 0;
 }
 
